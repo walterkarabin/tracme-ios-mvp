480a2abb59c79eed51510917b7c9b62d3d063d4d
Walter Karabin <walter.karabin@gmail.com>
2026-02-05 22:50:22 -0800

    Add TracMeAlpha app target and Xcode project.

---
+++ TracMeAlpha/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  tracme-alpha
+//
+//  Created by Walter Karabin on 2026-01-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
